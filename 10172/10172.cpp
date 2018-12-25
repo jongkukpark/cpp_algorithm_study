da38531b6e862bc96988f9c9a4d8cfab8043a714
jongkukpark <jongkukpark162@cnu.ac.kr>
2018-12-26 03:14:36 +0900

    10172번: 개 완료

---
+++ 10172/10172.cpp
@@ -0,0 +1,18 @@
+/*
+	백준 알고리즘 10172번: 개
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	cout << "|\\_/|" << endl;
+	cout << "|q p|   /}" << endl;
+	cout << "( 0 )\"\"\"\\" << endl;
+	cout << "|\"^\"`    |" << endl;
+	cout << "||_/=\\\\__|" << endl;
+	return 0;
+}
+
